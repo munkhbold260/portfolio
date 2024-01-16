4c077d2bb770c5a3d22d3afe2d127b69f7232048
bold_max <munkhbold260@gmail.com>
2024-01-16 23:05:19 +0800

    Portfolio started

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="max-w-1440 px-20 bg-red-100">
+    <div className="">
       <Portfolio />
     </div>
   );
