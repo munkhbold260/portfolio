fbcf9e0373c20aab80379d421405693a0075c541
Munkhbold Batmunkh <23LP2066@G401-CC0102.local>
2024-01-19 14:37:00 +0800

    dff

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -5,7 +5,10 @@ export const Experience = () => {
   return (
     <div className="bigContainer">
       <div className="smallContainer">
-        <p className="bg-red-300">Experience</p>
+        <p className="bg-gray-200 rounded-xl px-5 py-1 w-fit">Experience</p>
+        <p className="text-xl">
+          Here is a quick summary of my most recent experiences:
+        </p>
         <UpWork />
       </div>
     </div>
